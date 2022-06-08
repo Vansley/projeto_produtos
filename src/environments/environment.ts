11dd850f79698b52360f5646849c1d68dd4e86b2
Vansley <86673600+Vansley@users.noreply.github.com>
2022-06-08 13:52:24 -0300

    primeiro commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+//configurando um arquivo do projeto Angular
+// para armazenarioo URL do servidor da API.
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api'
+};
+
+
